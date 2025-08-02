c63588cc26f632ed23710504992bb8c56127b5f6
RENZO SUGANUMA <discoverygworks2023@gmail.com>
2025-08-02 14:50:44 +0900

    [update] 障害物が生成されるロジックの実装

--- Assets/Scripts/System/IniD/EnObstacleGen.cs
+++ Assets/Scripts/System/IniD/EnObstacleGen.cs
@@ -1,11 +1,12 @@
 using ImTipsyDude.InstantECS;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace ImTipsyDude.System.IniD
 {
     public class EnObstacleGen : IECSEntity
     {
         public GameObject[] Obstacles;
-        public LayerMask PLayerMask;
+        public LayerMask PlayerMask;
     }
 }
--- Assets/Scripts/System/IniD/SysObstacleGen.cs
+++ Assets/Scripts/System/IniD/SysObstacleGen.cs
@@ -5,6 +5,7 @@ using ImTipsyDude.InstantECS;
 using Unity.Mathematics;
 using UnityEditor;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace ImTipsyDude.System.IniD
 {
@@ -23,8 +24,22 @@ namespace ImTipsyDude.System.IniD
             var v = other.GetComponent<CmpIniDPlayer>();
             var apply = Math.Clamp(v.CurrentMaxSpeed * 2.0f, 0, v.MaxSpeed);
             v.CurrentMaxSpeed = apply;
-            
+
             transform.position += Vector3.forward * 10;
+            
+            GenerateObstacle();
+        }
+
+        private void GenerateObstacle()
+        {
+            var cmp = _enGen.GetComponent<CmpObstacleGen>();
+            var basepos = transform.position + (Vector3.forward * cmp.SpawnDistance);
+            var genLineStart = basepos + (Vector3.left * cmp.ColliderSize.x);
+            var genLineEnd = basepos + (Vector3.right * cmp.ColliderSize.x);
+
+            var genPos = Vector3.Lerp(genLineStart, genLineEnd, Random.Range(0.0f, 1.0f));
+
+            Instantiate(_enGen.Obstacles[Random.Range(0, _enGen.Obstacles.Length)], genPos, Quaternion.identity);
         }
     }
 }
