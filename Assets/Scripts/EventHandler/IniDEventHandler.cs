f06cc58d6fb52dac5ccc77c4d91310ed52bfa680
RENZO SUGANUMA <discoverygworks2023@gmail.com>
2025-08-04 22:12:27 +0900

    [update] うん

--- Assets/Scripts/EventHandler/IniDEventHandler.cs
+++ Assets/Scripts/EventHandler/IniDEventHandler.cs
@@ -1,8 +1,12 @@
+using System;
 using UnityEngine;
 
 namespace ImTipsyDude.IniD.Event
 {
     public class IniDEventHandler : MonoBehaviour
     {
+        public Action<Vector2> OnSlide;
+        public Action OnPlayerJump;
+        public Action OnPlayerCollide;
     }
 }
--- Assets/Scripts/MyInstantECS/GlobalEventPool.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using UnityEngine;
-
-public class GlobalEventPool : MonoBehaviour
-{
-    public Action<Vector2> OnSlide;
-    public Action OnPlayerJump;
-    public Action OnPlayerCollide;
-}
