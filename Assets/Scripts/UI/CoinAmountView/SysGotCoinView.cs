1dd552e14afed620277f321a729cf99a27e2e2a3
RENZO SUGANUMA <discoverygworks2023@gmail.com>
2025-08-05 16:20:43 +0900

    コイン増加イージング

--- Assets/Scripts/UI/CoinAmountView/SysGotCoinView.cs
+++ Assets/Scripts/UI/CoinAmountView/SysGotCoinView.cs
@@ -1,6 +1,8 @@
+using DG.Tweening;
 using ImTipsyDude.Helper;
 using ImTipsyDude.IniD.Event;
 using ImTipsyDude.InstantECS;
+using UnityEngine;
 
 
 public class SysGotCoinView : IECSSystem
@@ -15,15 +17,13 @@ public class SysGotCoinView : IECSSystem
 
         EnInstanceIdPool.Instance.Map.Add(nameof(SysGotCoinView), ID);
 
-        IniDEventHandler.Instance.OnTimeOut += () =>
-        {
-            IniDScoreManager.Instance.GotCoin = _cmpCoinView.GotCount;
-        };
+        IniDEventHandler.Instance.OnTimeOut += () => { IniDScoreManager.Instance.GotCoin = _cmpCoinView.GotCount; };
     }
 
     public void GotCoin()
     {
         _cmpCoinView.GotCount++;
         _enCoinView.Text.text = $"X{_cmpCoinView.GotCount}";
+        _enCoinView.Text.transform.DOPunchScale(Random.insideUnitCircle * 3, 0.5f);
     }
 }
