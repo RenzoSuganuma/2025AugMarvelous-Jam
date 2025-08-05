b7a8f9f935276f2dace5b0e3087e5e0ea2f38f4c
RENZO SUGANUMA <discoverygworks2023@gmail.com>
2025-08-05 10:53:27 +0900

    カウントダウン演出

--- Assets/Scripts/SceneManagement/Level1SceneEntity.cs
+++ Assets/Scripts/SceneManagement/Level1SceneEntity.cs
@@ -1,24 +1,48 @@
 using DG.Tweening;
 using ImTipsyDude.InstantECS;
+using TMPro;
 using UnityEngine;
 
 namespace ImTipsyDude.Scene
 {
     public class Level1SceneEntity : SceneEntity
     {
+        [SerializeField] private TMP_Text _countDownText;
+
         public override void OnStart()
         {
             IECSWorld.Instance.StartLoadNextScene("Exit");
             IECSWorld.Instance.UpdateInGameState(InGameState.Waiting);
 
             DOTween.Sequence()
-                .AppendCallback(() => Debug.Log("3"))
-                .AppendInterval(0.5f)
-                .AppendCallback(() => Debug.Log("2"))
-                .AppendInterval(0.5f)
-                .AppendCallback(() => Debug.Log("1"))
-                .AppendInterval(0.5f)
-                .AppendCallback(() => IECSWorld.Instance.UpdateInGameState(InGameState.Playing)).Play();
+                .AppendCallback(() =>
+                {
+                    _countDownText.rectTransform.DOPunchPosition(Random.insideUnitCircle * 5, 0.5f);
+                    _countDownText.text = "3";
+
+                    Debug.Log("3");
+                })
+                .AppendInterval(1f)
+                .AppendCallback(() =>
+                {
+                    _countDownText.rectTransform.DOPunchPosition(Random.insideUnitCircle * 7, 0.5f);
+                    _countDownText.text = "2";
+                    Debug.Log("2");
+                })
+                .AppendInterval(1f)
+                .AppendCallback(() =>
+                {
+                    _countDownText.rectTransform.DOPunchPosition(Random.insideUnitCircle * 10, 0.5f);
+                    _countDownText.text = "1";
+                    Debug.Log("1");
+                })
+                .AppendInterval(1f)
+                .AppendCallback(() =>
+                {
+                    _countDownText.text = string.Empty;
+                    IECSWorld.Instance.UpdateInGameState(InGameState.Playing);
+                })
+                .Play();
         }
 
         public override void OnUpdate()
